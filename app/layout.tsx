5b0ef650afc0475796bd8f5348dc59bab36bf0cd
Udoh Victory <victrudoh@gmail.com>
2024-12-12 01:16:30 +0100

    UPDATE: Added Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import GeneralProvider from "@/context/GenralContext";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Pays",
@@ -18,6 +19,30 @@ export default function RootLayout({
   return (
     <>
       <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        <head>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-X0WYX329V6`}
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X0WYX329V6');
+          `}
+          </Script>
+          {/* <!-- Google tag (gtag.js) --> */}
+          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0WYX329V6"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X0WYX329V6');
+          </script> */}
+        </head>
         <body>
           <GeneralProvider>
             <ToastContainer
