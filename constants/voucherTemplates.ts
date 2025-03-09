5543023af48e8d91100b8578e6d81dc8831405e7
CodedLibra <adeola.ogundijo@yahoo.com>
2025-03-09 15:03:53 +0100

    created voucherTemplates constant

---
+++ constants/voucherTemplates.ts
@@ -0,0 +1,87 @@
+export const voucherTemplates = [
+  {
+    title: "Happy Birthday",
+    scenario: "Happy birthday!",
+    description: "Celebrate your birthday with a little extra joy!",
+    voucherKey: "HAPBD",
+  },
+  {
+    title: "Happily Ever After",
+    scenario: "Happily Ever After!",
+    description: "A perfect start to your forever journey!",
+    voucherKey: "TONEA",
+  },
+  {
+    title: "Blessings to You",
+    scenario: "Ramadan & Easter",
+    description: "Wishing you joy and peace during this special season!",
+    voucherKey: "BUSTO",
+  },
+  {
+    title: "Welcome Little One",
+    scenario: "Newborns!",
+    description: "A special gift to celebrate your little bundle of joy!",
+    voucherKey: "WELLO",
+  },
+  {
+    title: "Season's Greetings",
+    scenario: "Christmas, New Year & Easter!",
+    description: "Sending warm wishes and festive joy your way!",
+    voucherKey: "SEAGR",
+  },
+  {
+    title: "You Did It!",
+    scenario: "Congratulations!",
+    description: "A reward for your hard work and success!",
+    voucherKey: "YODIT",
+  },
+  {
+    title: "Cheers to Love",
+    scenario: "Anniversaries!",
+    description: "Celebrating your love and commitment!",
+    voucherKey: "CHELO",
+  },
+  {
+    title: "For My Amazing Sibling",
+    scenario: "Siblings bond!",
+    description: "A small token of love for my forever friend!",
+    voucherKey: "FOWAS",
+  },
+  {
+    title: "Just Because",
+    scenario: "Thinking of you!",
+    description: "No reason needed - just a little something for you!",
+    voucherKey: "JUSTB",
+  },
+  {
+    title: "Truly Grateful",
+    scenario: "Thank You!",
+    description: "A heartfelt thank you for all that you do!",
+    voucherKey: "TRUSB",
+  },
+  {
+    title: "Well Done!",
+    scenario: "Employee Appreciation!",
+    description: "A small token of celebration for your achievement!",
+    voucherKey: "WELLD",
+  },
+  {
+    title: "Feel Better Soon",
+    scenario: "Get well soon!",
+    description: "Wishing you a speedy recovery and brighter days!",
+    voucherKey: "FBSOO",
+  },
+
+  {
+    title: "Happy Valentine's",
+    scenario: "Valentine's Day!",
+    description: "A special gift to celebrate love and friendship!",
+    voucherKey: "HAPVA",
+  },
+  {
+    title: "School Essentials",
+    scenario: "Back to school!",
+    description: "A little something to help with the new school year!",
+    voucherKey: "SCHOO",
+  },
+];
