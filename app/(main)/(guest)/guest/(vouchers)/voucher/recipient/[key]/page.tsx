2fec5fbda1fde299dcfd4c917c6f8bee147fb9df
CodedLibra <adeola.ogundijo@yahoo.com>
2025-03-25 19:20:54 +0100

    feat: created a voucher/recipient/[key] page

---
+++ app/(main)/(guest)/guest/(vouchers)/voucher/recipient/[key]/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { useGeneralContext } from "@/context/GenralContext";
+import Recipients from "@/app/(dashboard)/dashboard/vouchers/create/recipients/[key]/page";
+
+const page = () => {
+  // extract params from url
+  const params = useParams();
+  const { key } = params;
+
+  const {
+    oneGuestVoucherId,
+    oneVoucher,
+    getGuestTransactionById,
+    setOneGuestVoucherId,
+    setOneTransactionId,
+  }: any = useGeneralContext();
+
+  useEffect(() => {
+    getGuestTransactionById();
+    setOneGuestVoucherId(key);
+    setOneTransactionId(oneVoucher?.transactionId);
+  }, [key, oneGuestVoucherId, oneVoucher]);
+
+  return (
+    <div className="flex items-center justify-center md:h-screen mt-2 md:mt-0">
+      <Recipients params={oneVoucher?.specialKey} />
+    </div>
+  );
+};
+
+export default page;
