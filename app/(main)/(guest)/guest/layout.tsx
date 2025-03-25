ac4b7db1c0890805eb4946085c439d0b23c2b0a9
CodedLibra <adeola.ogundijo@yahoo.com>
2025-03-25 19:22:12 +0100

    added layout for guest routes

---
+++ app/(main)/(guest)/guest/layout.tsx
@@ -0,0 +1,21 @@
+import "@/app/globals.css";
+import SideNavBar from "@/components/Shared/SideNavBar";
+
+export default async function GuestLayout({
+  children,
+  isGuest = true,
+}: {
+  children: React.ReactNode;
+  isGuest?: boolean;
+}) {
+  return (
+    <div className="flex h-screen w-full overflow-x-hidden">
+      <SideNavBar isGuest={isGuest} />
+      <div className="flex flex-col w-full relative h-screen bg-brand-ash  overflow-y-scroll">
+        <div className="lg:mt-14 w-full p-4 h-[calc(100vh-80px)] ">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
