a2c97c9e7cd0882ead1cb5d5738471ef00f96793
CodedLibra <adeola.ogundijo@yahoo.com>
2025-03-29 01:03:28 +0100

    feat: added continue as a guest link

--- app/(main)/auth/login/page.tsx
+++ app/(main)/auth/login/page.tsx
@@ -114,30 +114,42 @@ const Login = () => {
                 )}
                 {/* divider */}
                 <div className="w-full h-[0.1px] mt-4 bg-brand-grayish"></div>
-                <span className="font-Manrope text-[#262626] text-base font-normal flex justify-around items-center gap-3 w-auto xl:w-[353px] tracking-tighter leading-5">
-                  If you don&apos;t have an account{" "}
-                  <Link
-                    href={"/auth/signup"}
-                    className="flex items-center text-[#3B82F6] text-base"
-                  >
-                    Signup Here{" "}
-                    <svg
-                      width="12"
-                      height="13"
-                      viewBox="0 0 12 13"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mx-2"
+                <div>
+                  <span className="font-Manrope text-[#262626] text-base font-normal flex justify-around items-center gap-3 w-auto xl:w-[353px] tracking-tighter leading-5">
+                    If you don&apos;t have an account{" "}
+                    <Link
+                      href={"/auth/signup"}
+                      className="flex items-center text-[#3B82F6] text-base"
                     >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M0.263225 12.2368C0.431987 12.4053 0.660751 12.5 0.899267 12.5C1.13778 12.5 1.36655 12.4053 1.53531 12.2368L10.1999 3.5722V10.4007C10.1999 10.6394 10.2947 10.8683 10.4635 11.0371C10.6323 11.2059 10.8612 11.3007 11.0999 11.3007C11.3387 11.3007 11.5676 11.2059 11.7364 11.0371C11.9052 10.8683 12 10.6394 12 10.4007V1.40006C12 1.16135 11.9052 0.932415 11.7364 0.763621C11.5676 0.594827 11.3387 0.5 11.0999 0.5H2.09935C1.86064 0.5 1.6317 0.594827 1.46291 0.763621C1.29411 0.932415 1.19929 1.16135 1.19929 1.40006C1.19929 1.63877 1.29411 1.8677 1.46291 2.0365C1.6317 2.20529 1.86064 2.30012 2.09935 2.30012H8.9278L0.263225 10.9647C0.0946735 11.1335 0 11.3622 0 11.6007C0 11.8392 0.0946735 12.068 0.263225 12.2368Z"
-                        fill="#3B82F6"
-                      />
-                    </svg>{" "}
-                  </Link>
-                </span>
+                      Signup Here{" "}
+                      <svg
+                        width="12"
+                        height="13"
+                        viewBox="0 0 12 13"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="mx-2"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M0.263225 12.2368C0.431987 12.4053 0.660751 12.5 0.899267 12.5C1.13778 12.5 1.36655 12.4053 1.53531 12.2368L10.1999 3.5722V10.4007C10.1999 10.6394 10.2947 10.8683 10.4635 11.0371C10.6323 11.2059 10.8612 11.3007 11.0999 11.3007C11.3387 11.3007 11.5676 11.2059 11.7364 11.0371C11.9052 10.8683 12 10.6394 12 10.4007V1.40006C12 1.16135 11.9052 0.932415 11.7364 0.763621C11.5676 0.594827 11.3387 0.5 11.0999 0.5H2.09935C1.86064 0.5 1.6317 0.594827 1.46291 0.763621C1.29411 0.932415 1.19929 1.16135 1.19929 1.40006C1.19929 1.63877 1.29411 1.8677 1.46291 2.0365C1.6317 2.20529 1.86064 2.30012 2.09935 2.30012H8.9278L0.263225 10.9647C0.0946735 11.1335 0 11.3622 0 11.6007C0 11.8392 0.0946735 12.068 0.263225 12.2368Z"
+                          fill="#3B82F6"
+                        />
+                      </svg>{" "}
+                    </Link>
+                  </span>
+                  <span className="font-Manrope text-[#262626] text-base font-normal flex justify-center items-center gap-1 w-auto xl:w-[353px] tracking-tighter leading-5">
+                    <span className="text-center font-bold">OR</span>
+                    Continue as a{" "}
+                    <Link
+                      href={"/guest/welcome"}
+                      className="flex items-center text-[#3B82F6] font-medium text-base"
+                    >
+                      Guest?
+                    </Link>
+                  </span>
+                </div>
               </div>
             </form>
             {/* right */}
