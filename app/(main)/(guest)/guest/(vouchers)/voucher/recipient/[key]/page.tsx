2dad522dc3c9ff45eede463062e7ee7dae4f5abd
CodedLibra <adeola.ogundijo@yahoo.com>
2025-03-31 13:53:02 +0100

    feat: ensured that guest Voucher datas are fetched by getGuestVoucherById function

--- app/(main)/(guest)/guest/(vouchers)/voucher/recipient/[key]/page.tsx
+++ app/(main)/(guest)/guest/(vouchers)/voucher/recipient/[key]/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 import { useParams } from "next/navigation";
 
 import { useGeneralContext } from "@/context/GenralContext";
-import Recipients from "@/app/(dashboard)/dashboard/vouchers/create/recipients/[key]/page";
+import Recipients from "@/app/(dashboard)/dashboard/vouchers/create/recipients/[key]/Recipients";
 
 const Page = () => {
   // extract params from url
@@ -14,6 +14,7 @@ const Page = () => {
   const {
     oneGuestVoucherId,
     oneVoucher,
+    getGuestVoucherById,
     getGuestTransactionById,
     setOneGuestVoucherId,
     setOneTransactionId,
@@ -23,10 +24,15 @@ const Page = () => {
     if (!oneGuestVoucherId) return;
 
     getGuestTransactionById();
-    setOneGuestVoucherId(key);
     setOneTransactionId(oneVoucher?.transactionId);
   }, [key, oneGuestVoucherId, oneVoucher]);
 
+  useEffect(() => {
+    if (!key) return;
+
+    setOneGuestVoucherId(key);
+    getGuestVoucherById();
+  }, [key, oneGuestVoucherId]);
   return (
     <div className="flex items-center justify-center md:h-screen mt-2 md:mt-0">
       <Recipients params={oneVoucher?.specialKey} isGuest={true} />
