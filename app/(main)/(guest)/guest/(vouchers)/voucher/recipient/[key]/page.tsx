8ec125505c494fc5590bfc716d5ef0925aadbd38
CodedLibra <adeola.ogundijo@yahoo.com>
2025-04-05 23:27:59 +0100

    feat: ensure transaction is paid before you can add a recipient and also getting status from transaction

--- app/(main)/(guest)/guest/(vouchers)/voucher/recipient/[key]/page.tsx
+++ app/(main)/(guest)/guest/(vouchers)/voucher/recipient/[key]/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { useGeneralContext } from "@/context/GenralContext";
 import Recipients from "@/app/(dashboard)/dashboard/vouchers/create/recipients/[key]/Recipients";
 
 const Page = () => {
+  const router = useRouter();
+
   // extract params from url
   const params = useParams();
   const { key } = params;
@@ -14,11 +16,17 @@ const Page = () => {
   const {
     oneGuestVoucherId,
     oneVoucher,
+    oneTransaction,
     getGuestVoucherById,
     getGuestTransactionById,
     setOneGuestVoucherId,
     setOneTransactionId,
   }: any = useGeneralContext();
+
+  // console.log(oneTransaction);
+
+  //Get transaction status
+  const { status } = oneTransaction;
 
   useEffect(() => {
     if (!oneGuestVoucherId) return;
@@ -27,12 +35,20 @@ const Page = () => {
     setOneTransactionId(oneVoucher?.transactionId);
   }, [key, oneGuestVoucherId, oneVoucher]);
 
+  //ensure transaction is paid before you can add a recipient
+  useEffect(() => {
+    if (status !== "PAID" || status !== "successful") {
+      router.push(`/guest/confirm-payment/${oneGuestVoucherId}`);
+    }
+  }, [status, oneGuestVoucherId]);
+
   useEffect(() => {
     if (!key) return;
 
     setOneGuestVoucherId(key);
     getGuestVoucherById();
   }, [key, oneGuestVoucherId]);
+
   return (
     <div className="flex items-center justify-center md:h-screen mt-2 md:mt-0">
       <Recipients params={oneVoucher?.specialKey} isGuest={true} />
